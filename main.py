3d50b5bce9169a1f8d039f2e2ede208dadf90932
Begi <102294103+saur0nnn@users.noreply.github.com>
2023-12-31 04:57:03 +0400

    works fine, need to add some more commands

--- main.py
+++ main.py
@@ -30,8 +30,10 @@ def write(text):
     with open(file_path, "w", encoding="utf-8") as file:
         file.write(text)
 
-
 commands = load_commands()
+
+is_jarvis_active = False
+jarvis_trigger = "ყლეო"
 
 
 while True:
@@ -43,24 +45,72 @@ while True:
         print(f"racxa errori: {e}")
 
     if spoken_text:
-        if "ჯარვის ჩართე" in spoken_text.lower():
-            for command in commands:
-                if command["trigger"] in spoken_text:
-                    try:
-                        open_app(command["application"], match_closest=True)
-                    except Exception as e:
-                        print(f"racxa errori: {e}")
-        elif "ჯარვის გამორთე" in spoken_text.lower():
-            for command in commands:
-                if command["trigger"] in spoken_text:
-                    try:
-                        close_app(command["application"], match_closest=True)
-                    except Exception as e:
-                        print(f"racxa errori: {e}")
-        elif "ჯარვის გაჩერდი" in spoken_text.lower():
-            print("jarvisma shewyvita mushaoba")
-            exit()
+        if jarvis_trigger in spoken_text:
+            is_jarvis_active = True
+
+
+            if "ჩართე" in spoken_text.lower():
+                for command in commands:
+                    if command["trigger"] in spoken_text:
+                        try:
+                            open_app(command["application"], match_closest=True)
+                        except Exception as e:
+                            print(f"racxa errori: {e}")
+            elif "გახსენი" in spoken_text.lower():
+                for command in commands:
+                    if command["trigger"] in spoken_text:
+                        try:
+                            open_app(command["application"], match_closest=True)
+                        except Exception as e:
+                            print(f"racxa errori: {e}")
+
+
+            elif "გამორთე" in spoken_text.lower():
+                for command in commands:
+                    if command["trigger"] in spoken_text:
+                        try:
+                            close_app(command["application"], match_closest=True)
+                        except Exception as e:
+                            print(f"racxa errori: {e}")
+            elif "გათიშე" in spoken_text.lower():
+                for command in commands:
+                    if command["trigger"] in spoken_text:
+                        try:
+                            close_app(command["application"], match_closest=True)
+                        except Exception as e:
+                            print(f"racxa errori: {e}")
+            elif "დახურე" in spoken_text.lower():
+                for command in commands:
+                    if command["trigger"] in spoken_text:
+                        try:
+                            close_app(command["application"], match_closest=True)
+                        except Exception as e:
+                            print(f"racxa errori: {e}")
+
+
+            elif "გაჩერდი" in spoken_text.lower():
+                print("jarvisma shewyvita mushaoba")
+                exit()
+            elif "ჩაიჯვი" in spoken_text.lower():
+                print("jarvisma shewyvita mushaoba")
+                exit()
+            else:
+                print("GAVIGE ISETI OPERATORI ROMELIC AR MISWAVLIA")
         else:
-            print("SHECDOMAAAAAAAAAAAAAAAA")
+            is_jarvis_active = False
     else:
-        print("tqveni brdzaneba araa chawerili")
+        print("warmoishva amoucnobi shecdoma")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
